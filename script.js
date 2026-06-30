245a0eca52f4e4432b7d593ca05574f96ca46061
asayilalshammary-star <asayilalshammary@gmail.com>
2026-06-30 18:55:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const envelope = document.getElementById("envelope");
+const coverPage = document.getElementById("coverPage");
+const invitePage = document.getElementById("invitePage");
+const inviteCard = document.querySelector(".invite-card");
+const music = document.getElementById("bgMusic");
+
+const items = document.querySelectorAll(".item");
+
+let opened = false;
+
+envelope.addEventListener("click", () => {
+    if (opened) return;
+    opened = true;
+
+    music.volume = 0.8;
+    music.play().catch(() => {});
+
+    envelope.classList.add("open");
+
+    setTimeout(() => {
+        coverPage.style.display = "none";
+        invitePage.classList.remove("hidden");
+
+        setTimeout(() => {
+            invitePage.classList.add("show");
+            inviteCard.classList.add("show");
+        }, 100);
+
+        setTimeout(() => {
+            showItemsSlowly();
+        }, 1300);
+
+    }, 950);
+});
+
+function showItemsSlowly(){
+    let delay = 0;
+
+    items.forEach((item) => {
+        setTimeout(() => {
+            item.classList.add("show");
+        }, delay);
+
+        delay += 1100; // سرعة ظهور الأسطر: زيدي الرقم لو تبينه أبطأ
+    });
+}
